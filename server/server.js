c24b5efe7068d9219610754f1ef3278c55f1df01
Zac Edens <themancalledzac@gmail.com>
2021-02-19 16:28:02 -0800

    added server.js to the server files

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+// const logger = require("morgan");
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+// app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/menu", {
+  useNewUrlParser: true,
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
