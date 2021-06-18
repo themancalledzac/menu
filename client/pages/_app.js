0cfa2870ade297595476c2a2cd34504f51918f85
Taani Maama <taani1maama@live.com>
2021-06-18 11:46:04 -0700

    Updated the app.js and document.js so they work with MaterialUI. Then I also removed Wes' eslint behavior.

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,14 +1,40 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react/jsx-props-no-spreading */
-import Page from "../components/Page";
-import "../styles/globals.css";
+import { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../styles/theme';
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
+
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <>
+      <Head>
+        <title>My page</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
 
-export default MyApp;
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired
+};
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,13 +1,17 @@
-export default function Home() {
+import Grid from '@material-ui/core/Grid';
+
+export function Home() {
   return (
     <main>
-      <div id="jumbotron-banner">
-        <h2>Menu is a online restaurant management system.</h2>
-        <h3>
-          Build your system and have customer build their own items with your
-          ingredients or have them pick for your Menu.
-        </h3>
-      </div>
+      <Grid>
+        <div id="jumbotron-banner">
+          <h2>Menu is a online restaurant management system.</h2>
+          <h3>
+            Build your system and have customer build their own items with your
+            ingredients or have them pick for your Menu.
+          </h3>
+        </div>
+      </Grid>
     </main>
   );
 }
