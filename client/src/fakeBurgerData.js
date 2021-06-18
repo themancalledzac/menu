37e339bc969b2019300d15f35bd72528aba33464
Zac Edens <themancalledzac@gmail.com>
2021-06-18 16:59:55 -0700

    updated create page, mapped fake data for now. included a lot of styling notes for reference. Curious about package uploads, and if those need to be standardized for all of us. Also included an iphoneX design png

---
+++ client/src/fakeBurgerData.js
@@ -0,0 +1,106 @@
+const fakeBurgerData = [
+  {
+    name: "Beef",
+    price: 2.5,
+    description: "Made from cow yo",
+    stock: 60,
+    vegitarian: false,
+  },
+  {
+    name: "chicken",
+    price: 3.0,
+    description: "Made from chicken yo",
+    stock: 40,
+    vegitarian: false,
+  },
+  {
+    name: "pork katsu",
+    price: 5.0,
+    description: "Made from pig yo",
+    stock: 30,
+    vegitarian: false,
+  },
+  {
+    name: "beef katsu",
+    price: 5.0,
+    description: "Made from cow yo",
+    stock: 30,
+    vegitarian: false,
+  },
+  {
+    name: "Impossible",
+    price: 5.0,
+    description: "Made from something yo",
+    stock: 30,
+    vegitarian: true,
+  },
+  {
+    name: "Impossible katsu",
+    price: 5.0,
+    description: "Made from something yo",
+    stock: 30,
+    vegitarian: true,
+  },
+  {
+    name: "bun",
+    price: 0.5,
+    stock: 100,
+  },
+  {
+    name: "onion",
+    price: 0.2,
+    stock: 100,
+  },
+  {
+    name: "pickle",
+    price: 0.1,
+    stock: 100,
+  },
+  {
+    name: "lettuce",
+    price: 0.2,
+    stock: 100,
+  },
+  {
+    name: "coleslaw",
+    price: 0.2,
+    stock: 100,
+  },
+  {
+    name: "kimchi",
+    price: 0.2,
+    stock: 100,
+  },
+  {
+    name: "arugula",
+    price: 0.2,
+    stock: 100,
+  },
+  {
+    name: "tomato",
+    price: 0.3,
+    stock: 100,
+  },
+  {
+    name: "avocado",
+    price: 1.0,
+    stock: 100,
+  },
+  {
+    name: "bacon",
+    price: 1.2,
+    stock: 100,
+  },
+  {
+    name: "mushrooms",
+    price: 0.3,
+    stock: 100,
+  },
+  {
+    name: "egg",
+    price: 0.8,
+    stock: 100,
+  },
+];
+
+export default fakeBurgerData;
