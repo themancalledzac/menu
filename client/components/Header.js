185cf6c9e6e6d7aab597ae59286a8dcbc483c17f
Zac Edens <edens.zac@gmail.com>
2021-09-04 17:41:17 -0700

    updated header spacing, initialized signin and signup components

--- client/components/Header.js
+++ client/components/Header.js
@@ -10,18 +10,23 @@ export default function Header() {
       grid-template-columns: auto 1fr;
       justify-content: space-between;
       align-items: stretch;
+      background-color: blueviolet;
     }
     .sub-bar {
       display: grid;
       grid-template-columns: 1fr auto;
       border-bottom: 1px solid black;
     }
+
+    .title {
+      margin-left: 3rem;
+    }
   `;
 
   return (
     <HeaderStyles>
       <div className='bar'>
-        <h1>
+        <h1 className='title'>
           <Link href='/'>MENU</Link>
         </h1>
         <Nav />
