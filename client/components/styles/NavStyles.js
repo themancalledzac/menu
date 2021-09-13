48cd5c3486ae2ae443e47f383ed1545fe1371aac
Zac Edens <edens.zac@gmail.com>
2021-09-13 10:40:52 -0700

    updated fsvents part 4

---
+++ client/components/styles/NavStyles.js
@@ -0,0 +1,16 @@
+const NavStyles = styled.ul`
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-self: end;
+  font-size: 2rem;
+  a,
+  button {
+    padding: 1rem 3rem;
+    display: flex;
+    align-items: center;
+    position: relative;
+    text-transform: uppercase;
+    font-weight: 900;
+  }
+`;
