800f70e2afefd2418dbdd6f6ce26c2290b1d659a
Zac Edens <edens.zac@gmail.com>
2021-09-22 10:24:57 -0700

    updated page component width to be width dynamic.

--- client/components/Page.js
+++ client/components/Page.js
@@ -4,6 +4,33 @@ import MobileHeader from "../components/MobileHeader";
 import DesktopHeader from "../components/DesktopHeader";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import ScrollTitle from "./ScrollTitle";
+import { Paper } from "@mui/material";
+import { styled } from "@mui/system";
+
+const MainPageDiv = styled("div")(({ theme }) => ({
+  maxWidth: "95%",
+  marginLeft: "auto",
+  marginRight: "auto",
+  backgroundColor: "rgba(255, 255, 255, .5)",
+  alignContent: "center",
+  display: "flex",
+  justifyContent: "center",
+}));
+
+const MainPaper = styled(Paper)(({ theme }) => ({
+  [theme.breakpoints.up("sm")]: {
+    alignContent: "center",
+    maxWidth: "85%",
+    marginTop: "2rem",
+    width: "50rem",
+  },
+  [theme.breakpoints.down("sm")]: {
+    alignContent: "center",
+    maxWidth: "97%",
+    marginTop: "2rem",
+    width: "50rem",
+  },
+}));
 
 export default function Page({ children }) {
   const theme = useTheme();
@@ -13,7 +40,9 @@ export default function Page({ children }) {
     <div>
       {/* {!matches && <ScrollTitle />} */}
       <DesktopHeader />
-      <div>{children}</div>
+      <MainPageDiv>
+        <MainPaper>{children}</MainPaper>
+      </MainPageDiv>
       {!matches && <MobileHeader />}
     </div>
   );
